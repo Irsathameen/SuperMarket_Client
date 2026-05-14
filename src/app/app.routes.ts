72fb09939b99731c29745cc3ad53b03dac9a87a0
irsathameen <irsathameen@gmail.com>
2026-05-14 08:23:00 +0530

    Login page has been added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Login } from './auth/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',component:Login
+        
+    }
+
+];
